d4325e4be78cf37780e48c930c93060ec3f541d3
Simone Parente <parentesimone.01@gmail.com>
2023-03-23 17:26:12 +0100

    esercizi 1-2

--- Esercizio1/main.c
+++ Esercizio1/main.c
@@ -8,8 +8,47 @@ indice di posizione di v nel vettore), rimuova v da H, mantenendo la proprietà 
 
 #define MAX 10
 
+void swap(int *a, int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+}
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1; // sinistro = 2*i + 1
+    int r = 2 * i + 2; // destro = 2*i + 2
+
+    // Verifica se il figlio sinistro è più grande del padre
+    if (l < n && arr[i] < arr[l])
+        largest = l;
+
+    // Verifica se il figlio destro è più grande del padre o del 
+		//figlio sinistro
+    if (r < n && arr[largest] < arr[r])
+        largest = r;
+
+    // Se il padre non è il più grande, scambia il padre con il 
+		//figlio più grande e richiama heapify per il figlio appena scambiato
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        heapify(arr, n, largest);
+    }
+}
+
 void funzione(int *H, int v, int indice){
-    
+    swap(&H[indice], &H[MAX-1]);
+    H[MAX-1]=0;
+    heapify(H, MAX, 1);
 }
 
 int main(){
@@ -17,8 +56,9 @@ int main(){
     int H[MAX]={42, 37, 40, 25, 20, 3, 20, 7, 18, 1};
     int i=(rand() % (MAX));
     int v=H[i];
-
+    printArray(H, MAX);
+    printf("Elemento da rimuovere: {%d}, H[%d]\n", v, i);
     funzione(H, v, i);
-
+    printArray(H, MAX);
 
 }
