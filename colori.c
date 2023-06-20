62d06f955bbe4a3ca32f909e2e6ac22259288b31
Simone Parente <parentesimone.01@gmail.com>
2023-06-20 20:38:48 +0200

    define colori

---
+++ colori.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TEXTCOLOR_DEFAULT "\x1b[m"
+#define TEXTCOLOR_RED "\x1b[31m"
+#define TEXTCOLOR_GREEN "\x1b[32m"
+#define TEXTCOLOR_YELLOW "\x1b[33m"
+#define TEXTCOLOR_BLUE "\x1b[34m"
+#define TEXTCOLOR_MAGENTA "\x1b[35m"
+#define TEXTCOLOR_CYAN "\x1b[36m"
+#define TEXTCOLOR_WHITE "\x1b[37m"
+#define TEXTCOLOR_BLACK "\x1b[30m"
